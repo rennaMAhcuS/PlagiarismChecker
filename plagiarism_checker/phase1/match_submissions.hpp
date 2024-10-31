1d4c4ba7eea316ab5feee81b9e7a44bfa0ca2994
rennaMAhcuS <rennaMAhcuS@users.noreply.github.com>
2024-11-01 00:49:00 +0530

    KMPTable implemented

--- plagiarism_checker/phase1/match_submissions.hpp
+++ plagiarism_checker/phase1/match_submissions.hpp
@@ -11,8 +11,32 @@
 
 // OPTIONAL: Add your helper functions and data structures here
 
-std::array<int, 5> match_submissions(std::vector<int> &submission1,
-                                     std::vector<int> &submission2) {
+std::vector<int> kmp_table(std::string&& s) {
+    int i = 1, j = 0;
+    std::vector<int> h(s.size() + 1);
+    h[0] = -1;
+    while (i < s.size()) {
+        if (s[j] != s[i]) {
+            h[i] = j;
+            while (j >= 0 && s[j] != s[i]) j = h[j];
+        } else
+            h[i] = h[j];
+        i++, j++;
+    }
+    h[s.size()] = j;
+    return h;
+}
+
+std::vector<std::vector<int>> allKmps(std::string& s) {
+    std::vector<std::vector<int>> res(s.size(), std::vector<int>());
+    for (int i = 0; i < s.size(); i++) {
+        res[i] = kmp_table(s.substr(i));
+    }
+    return res;
+}
+
+std::array<int, 5> match_submissions(std::vector<int>& submission1,
+                                     std::vector<int>& submission2) {
     // TODO: Write your code here
     std::array<int, 5> result = {0, 0, 0, 0, 0};
     return result;  // dummy return
