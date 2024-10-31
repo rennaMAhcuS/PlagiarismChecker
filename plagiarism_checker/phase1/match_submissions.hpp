26b5cf9f2eba210a3d7d95f6d407173711e90371
rennaMAhcuS <rennaMAhcuS@users.noreply.github.com>
2024-11-01 00:50:35 +0530

    Number of matches added

--- plagiarism_checker/phase1/match_submissions.hpp
+++ plagiarism_checker/phase1/match_submissions.hpp
@@ -11,7 +11,7 @@
 
 // OPTIONAL: Add your helper functions and data structures here
 
-std::vector<int> kmp_table(std::string&& s) {
+std::vector<int> kmp_table(const std::vector<int>&& s) {
     int i = 1, j = 0;
     std::vector<int> h(s.size() + 1);
     h[0] = -1;
@@ -27,12 +27,44 @@ std::vector<int> kmp_table(std::string&& s) {
     return h;
 }
 
-std::vector<std::vector<int>> allKmps(std::string& s) {
+std::vector<std::vector<int>> allKmps(const std::vector<int>& s) {
     std::vector<std::vector<int>> res(s.size(), std::vector<int>());
     for (int i = 0; i < s.size(); i++) {
-        res[i] = kmp_table(s.substr(i));
+        res[i] = kmp_table(std::vector<int>(s.begin() + i, s.end()));
     }
     return res;
+}
+
+std::vector<int> kmp(const std::vector<int>& s, const std::vector<int>& p, const std::vector<int>& h) {
+    if (p.size() <= 0) throw std::invalid_argument("Pattern is empty");
+    int i = 0, j = 0;
+    std::vector<int> res;
+    res.reserve(s.size());
+    while (i < s.size()) {
+        if (p[j] == s[i]) {
+            i++, j++;
+            if (j == p.size()) {
+                res.push_back(i - j);
+                j = h[j];
+            }
+        } else {
+            j = h[j];
+            if (j < 0) i++, j++;
+        }
+    }
+    return res;
+}
+
+int numMatches(const std::vector<int>& v1, const std::vector<int>& v2) {
+    std::vector<std::vector<int>> kmps = allKmps(v1);
+    int n = v1.size(), m = v2.size(), num = 0;
+    for (int i = n - 1; i >= 0; i--) {
+        for (int j = 0; j < n - i; j++) {
+            std::vector<int> res = kmp(v2, std::vector<int>(v1.begin() + j, v1.begin() + j + i + 1), kmps[j]);
+            num += res.size();
+        }
+    }
+    return nums;
 }
 
 std::array<int, 5> match_submissions(std::vector<int>& submission1,
