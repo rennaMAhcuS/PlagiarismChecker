b9e00c1abb9d6a772be65b01814ea807b922445f
awezmehtab <awezmehtab@users.noreply.github.com>
2024-11-01 18:57:35 +0530

    test.cpp changed

--- plagiarism_checker/phase1/test.cpp
+++ plagiarism_checker/phase1/test.cpp
@@ -1,16 +1,16 @@
 #include "../tokenizer.hpp"
 #include "match_submissions.hpp"
 
-void printVec(std::vector<int>& v) {
-    for (int i = 0; i < v.size(); i++) {
-        std::cout << v[i] << " ";
-    }
-    std::cout << "\n";
-}
+// void printVec(std::vector<int>& v) {
+//     for (int i = 0; i < v.size(); i++) {
+//         std::cout << v[i] << " ";
+//     }
+//     std::cout << "\n";
+// }
 
 int main(void) {
-    std::string s = "xyxy";
-    std::vector<std::vector<int>> v = allKmps(s);
-    for (auto& i : v) printVec(i);
-    std::cout << "Hello beta\n";
+    tokenizer_t file_one("testcases/one/one.cpp");
+    tokenizer_t file_two("testcases/one/two.cpp");
+    std::vector<int> v1 = file_one.get_tokens();
+    std::vector<int> v2 = file_two.get_tokens();
 }
