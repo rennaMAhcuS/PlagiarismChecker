898f3fb97ca0f6f2850d433f0775dc637637f979
rennaMAhcuS <rennaMAhcuS@users.noreply.github.com>
2024-11-09 20:00:16 +0530

    Added Makefile CLI file specification support, Updated README.md and Refactoring

---
+++ phase3/main.cpp
@@ -0,0 +1,58 @@
+#include <iomanip>
+
+#include "tokenizer.hpp"
+
+#ifdef CHECKER_NUMBER
+#if CHECKER_NUMBER == 0
+#include "checkers/checker_zero.hpp"
+#elif CHECKER_NUMBER == 1
+#include "checkers/checker_one.hpp"
+#elif CHECKER_NUMBER == 2
+#include "checkers/checker_two.hpp"
+#elif CHECKER_NUMBER == 3
+#include "checkers/checker_three.hpp"
+#elif CHECKER_NUMBER == 4
+#include "checkers/checker_four.hpp"
+#elif CHECKER_NUMBER == 5
+#include "checkers/checker_five.hpp"
+#else
+#error "Invalid CHECKER_NUMBER"
+#endif
+#endif
+
+// You should NOT modify ANYTHING in this file.
+extern std::array<int, 5> match_submissions(std::vector<int> &submission1,
+                                            std::vector<int> &submission2);
+
+void execute_and_verify_testcase(std::string test_dir) {
+    tokenizer_t file_one(test_dir + "/one.cpp");
+    tokenizer_t file_two(test_dir + "/two.cpp");
+    std::vector<int> submission1 = file_one.get_tokens();
+    std::vector<int> submission2 = file_two.get_tokens();
+    std::array<int, 5> output = match_submissions(submission1, submission2);
+
+    std::ifstream in(test_dir + "/expected.txt");
+    std::array<int, 5> expected;
+    in >> expected[0] >> expected[1] >> expected[2] >>
+        expected[3] >> expected[4];
+    in.close();
+
+    for (int i = 0; i < 5; i++) {
+        std::cout << "Result " << i << ":\t";
+        std::cout << "Your output: " << std::setw(10) << std::left << output[i];
+        std::cout << "\tSample output: " << expected[i] << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+int main(void) {
+    std::cout << "Testcase 1: " << std::endl;
+    execute_and_verify_testcase("testcases/one");
+
+    std::cout << "Testcase 2: " << std::endl;
+    execute_and_verify_testcase("testcases/two");
+
+    std::cout << "Testcase 3: " << std::endl;
+    execute_and_verify_testcase("testcases/three");
+    return 0;
+}
