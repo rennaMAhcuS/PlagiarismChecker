c0e64b0abcad8a091031fdb15bb845f80c328047
Mohana Evuri <148174808+rennaMAhcuS@users.noreply.github.com>
2024-11-24 21:01:40 +0530

    Updated `gameChanger` code

--- phase2/plagiarism_checker.cpp
+++ phase2/plagiarism_checker.cpp
@@ -1,18 +1,27 @@
+// plagiarism_checker.cpp
+
 #include "plagiarism_checker.hpp"
 
-// TODO: Implement the methods of the plagiarism_checker_t class
+// Implement the methods of the plagiarism_checker_t class
 
-plagiarism_checker_t::plagiarism_checker_t() : stop(false) {
+plagiarism_checker_t::plagiarism_checker_t() : stop(false), max_threads(3) {
     submission_processor_thread = std::thread(&plagiarism_checker_t::submission_processor, this);
+    for (size_t i = 0; i < max_threads; ++i) {
+        thread_pool.emplace_back(&plagiarism_checker_t::worker_thread, this);
+    }
 }
 
-plagiarism_checker_t::plagiarism_checker_t(std::vector<std::shared_ptr<submission_t>> __submissions) : stop(false) {
-    int curr_time = curr_time_millis();
+plagiarism_checker_t::plagiarism_checker_t(std::vector<std::shared_ptr<submission_t>> __submissions)
+    : stop(false), max_threads(3) {
+    int64_t curr_time = curr_time_millis();
     for (auto& submission : __submissions) {
-        submissions_list.push_back({curr_time, submission, false});
+        submissions_list.emplace_back(curr_time, submission, false);
         tokens_cache[submission->id] = tokenizer_t(submission->codefile).get_tokens();
     }
     submission_processor_thread = std::thread(&plagiarism_checker_t::submission_processor, this);
+    for (size_t i = 0; i < max_threads; ++i) {
+        thread_pool.emplace_back(&plagiarism_checker_t::worker_thread, this);
+    }
 }
 
 plagiarism_checker_t::~plagiarism_checker_t() {
@@ -24,7 +33,11 @@ plagiarism_checker_t::~plagiarism_checker_t() {
     if (submission_processor_thread.joinable()) {
         submission_processor_thread.join();
     }
-    for (auto& thread : comparison_threads) {
+    {
+        std::unique_lock<std::mutex> lock(tasks_mutex);
+        tasks_cv.notify_all();
+    }
+    for (auto& thread : thread_pool) {
         if (thread.joinable()) {
             thread.join();
         }
@@ -46,126 +59,184 @@ void plagiarism_checker_t::submission_processor() {
             std::unique_lock<std::mutex> lock(queue_mutex);
             cv.wait(lock, [this] { return stop || !submission_queue.empty(); });
             if (stop && submission_queue.empty()) {
-                return;
+                break;
             }
             submission = submission_queue.front();
             submission_queue.pop();
         }
 
-        int curr_time = curr_time_millis();
+        int64_t curr_time = curr_time_millis();
         std::vector<int> curr_tokens = tokenizer_t(submission->codefile).get_tokens();
         int curr_index;
         {
             std::lock_guard<std::mutex> lock(queue_mutex);
-            submissions_list.push_back({curr_time, submission, false});
+            submissions_list.emplace_back(curr_time, submission, false);
             tokens_cache[submission->id] = curr_tokens;
             curr_index = submissions_list.size() - 1;
         }
 
-        // Limit to 2 comparison threads
-        while (true) {
-            {
-                std::lock_guard<std::mutex> lock(queue_mutex);
-                // Remove finished threads
-                for (auto it = comparison_threads.begin(); it != comparison_threads.end();) {
-                    if (it->joinable()) {
-                        it->join();
-                        it = comparison_threads.erase(it);
-                    } else {
-                        ++it;
-                    }
-                }
-                if (comparison_threads.size() < 2) {
-                    break;
-                }
-            }
-            std::this_thread::sleep_for(std::chrono::milliseconds(10));
-        }
-
-        // Launch comparison in a new thread
+        // Add the comparison task to the tasks queue
         {
-            std::lock_guard<std::mutex> lock(queue_mutex);
-            comparison_threads.emplace_back(&plagiarism_checker_t::compare_submissions, this, curr_index);
+            std::lock_guard<std::mutex> lock(tasks_mutex);
+            tasks_queue.push(curr_index);
         }
+        tasks_cv.notify_one();
+    }
+}
+
+void plagiarism_checker_t::worker_thread() {
+    while (true) {
+        int task_index;
+        {
+            std::unique_lock<std::mutex> lock(tasks_mutex);
+            tasks_cv.wait(lock, [this] { return stop || !tasks_queue.empty(); });
+            if (stop && tasks_queue.empty()) {
+                break;
+            }
+            if (!tasks_queue.empty()) {
+                task_index = tasks_queue.front();
+                tasks_queue.pop();
+            } else {
+                continue;
+            }
+        }
+        compare_submissions(task_index);
     }
 }
 
 void plagiarism_checker_t::compare_submissions(int curr_index) {
-    auto& [curr_time, curr_submission, curr_flagged] = submissions_list[curr_index];
-    auto& curr_tokens = tokens_cache[curr_submission->id];
+    std::shared_ptr<submission_t> curr_submission;
+    std::vector<int> curr_tokens;
+
+    {
+        std::lock_guard<std::mutex> lock(queue_mutex);
+        int64_t curr_time = std::get<0>(submissions_list[curr_index]);
+        curr_submission = std::get<1>(submissions_list[curr_index]);
+        curr_tokens = tokens_cache[curr_submission->id];
+    }
+
     int matches_last_second = 0;
 
     for (int i = 0; i < curr_index; ++i) {
-        auto& [prev_time, prev_submission, prev_flagged] = submissions_list[i];
-        if (prev_time < curr_time - 1000) {
-            continue;
-        }
         check_two_submissions(curr_index, i, matches_last_second);
     }
 
-    if (!std::get<2>(submissions_list[curr_index]) && matches_last_second >= 20) {
+    bool should_flag = false;
+    {
+        std::lock_guard<std::mutex> lock(queue_mutex);
+        should_flag = !std::get<2>(submissions_list[curr_index]);
+    }
+
+    if (should_flag && matches_last_second >= 20) {
         {
             std::lock_guard<std::mutex> lock(queue_mutex);
             std::get<2>(submissions_list[curr_index]) = true;
         }
-        curr_submission->student->flag_student(curr_submission);
-        curr_submission->professor->flag_professor(curr_submission);
+
+        if (curr_submission->student) {
+            curr_submission->student->flag_student(curr_submission);
+        }
+        if (curr_submission->professor) {
+            curr_submission->professor->flag_professor(curr_submission);
+        }
     }
 }
 
 void plagiarism_checker_t::check_two_submissions(int curr_index, int prev_index, int& matches_last_second) {
-    auto& [curr_time, curr_submission, curr_flagged] = submissions_list[curr_index];
-    auto& [prev_time, prev_submission, prev_flagged] = submissions_list[prev_index];
+    std::shared_ptr<submission_t> curr_submission;
+    std::shared_ptr<submission_t> prev_submission;
+    std::vector<int> curr_tokens;
+    std::vector<int> prev_tokens;
+    int64_t curr_time;
+    int64_t prev_time;
 
-    auto& curr_tokens = tokens_cache[curr_submission->id];
-    auto& prev_tokens = tokens_cache[prev_submission->id];
+    {
+        std::lock_guard<std::mutex> lock(queue_mutex);
+        curr_time = std::get<0>(submissions_list[curr_index]);
+        curr_submission = std::get<1>(submissions_list[curr_index]);
+        curr_tokens = tokens_cache[curr_submission->id];
+
+        prev_time = std::get<0>(submissions_list[prev_index]);
+        prev_submission = std::get<1>(submissions_list[prev_index]);
+        prev_tokens = tokens_cache[prev_submission->id];
+    }
 
     int num_matches = 0;
     int max_match_length = 0;
-
     int curr_size = curr_tokens.size();
     int prev_size = prev_tokens.size();
 
-    std::vector<int> prev_row(prev_size + 1, 0), curr_row(prev_size + 1, 0);
+    if (curr_size == 0 || prev_size == 0) {
+        return;
+    }
+
+    std::vector<int> prev_row(prev_size + 1, 0);
     for (int i = 1; i <= curr_size; ++i) {
+        std::vector<int> curr_row(prev_size + 1, 0);
         for (int j = 1; j <= prev_size; ++j) {
             if (curr_tokens[i - 1] == prev_tokens[j - 1]) {
                 curr_row[j] = prev_row[j - 1] + 1;
-                if (curr_row[j] == 15) num_matches++;
+                if (curr_row[j] == 15) {
+                    num_matches++;
+                }
                 max_match_length = std::max(max_match_length, curr_row[j]);
-            } else {
-                curr_row[j] = 0;
             }
         }
-        std::swap(curr_row, prev_row);
+        prev_row.swap(curr_row);
     }
 
     if (num_matches >= 10 || max_match_length >= 75) {
+        int64_t time_diff = curr_time - prev_time;
+        bool flag_both = (std::abs(time_diff) < 1000);
+
+        bool curr_already_flagged = false;
+        bool prev_already_flagged = false;
+
         {
             std::lock_guard<std::mutex> lock(queue_mutex);
-            if (!std::get<2>(submissions_list[prev_index])) {
-                std::get<2>(submissions_list[prev_index]) = true;
-            }
-            if (!std::get<2>(submissions_list[curr_index])) {
+            curr_already_flagged = std::get<2>(submissions_list[curr_index]);
+            if (!curr_already_flagged) {
                 std::get<2>(submissions_list[curr_index]) = true;
             }
+            if (flag_both) {
+                prev_already_flagged = std::get<2>(submissions_list[prev_index]);
+                if (!prev_already_flagged) {
+                    std::get<2>(submissions_list[prev_index]) = true;
+                }
+            }
         }
-        prev_submission->student->flag_student(prev_submission);
-        prev_submission->professor->flag_professor(prev_submission);
-        curr_submission->student->flag_student(curr_submission);
-        curr_submission->professor->flag_professor(curr_submission);
+
+        // Flag current submission if not already flagged
+        if (!curr_already_flagged) {
+            if (curr_submission->student) {
+                curr_submission->student->flag_student(curr_submission);
+            }
+            if (curr_submission->professor) {
+                curr_submission->professor->flag_professor(curr_submission);
+            }
+        }
+
+        // Flag previous submission if within 1 second and not already flagged
+        if (flag_both && !prev_already_flagged) {
+            if (prev_submission->student) {
+                prev_submission->student->flag_student(prev_submission);
+            }
+            if (prev_submission->professor) {
+                prev_submission->professor->flag_professor(prev_submission);
+            }
+        }
     }
 
-    if (prev_time >= curr_time - 1000) {
+    if ((curr_time - prev_time) <= 1000) {
         matches_last_second += num_matches;
     }
 }
 
-int plagiarism_checker_t::curr_time_millis() {
+int64_t plagiarism_checker_t::curr_time_millis() {
     auto now = std::chrono::steady_clock::now();
     auto duration = now.time_since_epoch();
     auto curr_time = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
-    return static_cast<int>(curr_time);
+    return curr_time;
 }
 
-// End TODO
+// End of implementation
--- phase2/plagiarism_checker.hpp
+++ phase2/plagiarism_checker.hpp
@@ -1,3 +1,5 @@
+// plagiarism_checker.hpp
+
 #include <atomic>
 #include <chrono>
 #include <condition_variable>
@@ -8,7 +10,8 @@
 
 #include "structures.hpp"
 
-// You are free to add any STL includes above this comment, below the --line--.
+// You should NOT modify ANYTHING in this file above this comment.
+// You are free to add any STL includes above, below the --line--.
 // DO NOT add "using namespace std;" or include any other files/libraries.
 // Also DO NOT add the include "bits/stdc++.h"
 
@@ -16,18 +19,17 @@
 
 class plagiarism_checker_t {
    public:
-    plagiarism_checker_t(void);
+    plagiarism_checker_t();
     plagiarism_checker_t(std::vector<std::shared_ptr<submission_t>> __submissions);
-    ~plagiarism_checker_t(void);
+    ~plagiarism_checker_t();
     void add_submission(std::shared_ptr<submission_t> __submission);
 
    protected:
-    // Members and function signatures
     std::queue<std::shared_ptr<submission_t>> submission_queue;
-    std::vector<std::tuple<int, std::shared_ptr<submission_t>, bool>> submissions_list;
+    std::vector<std::tuple<int64_t, std::shared_ptr<submission_t>, bool>> submissions_list;
     std::unordered_map<long, std::vector<int>> tokens_cache;
+
     std::thread submission_processor_thread;
-    std::vector<std::thread> comparison_threads;
     std::atomic<bool> stop;
     std::mutex queue_mutex;
     std::condition_variable cv;
@@ -35,7 +37,13 @@ class plagiarism_checker_t {
     void submission_processor();
     void compare_submissions(int curr_index);
     void check_two_submissions(int curr_index, int prev_index, int& matches_last_second);
-    int curr_time_millis();
+    int64_t curr_time_millis();
 
-    // End TODO
+    // Thread pool management
+    size_t max_threads;
+    std::vector<std::thread> thread_pool;
+    std::queue<int> tasks_queue;
+    std::mutex tasks_mutex;
+    std::condition_variable tasks_cv;
+    void worker_thread();
 };
